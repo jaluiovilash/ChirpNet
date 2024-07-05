0fbb78ce9db04e2176172fc82616662c878231a7
jaluiovilash <ovilashjalui@gmail.com>
2024-07-05 12:39:28 +0530

    Remove all files from the repository/did wrong commit

--- chirp/chirpnet/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ChirpNet
-
-# Register your models here.
-
-admin.site.register(ChirpNet)
--- chirp/chirpnet/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import ChirpNet
-
-class ChirpNetForm(forms.ModelForm):
-    # imp - syntax for creating a new form (Meta)
-    class Meta:    
-        model=ChirpNet
-        fields = ['text', 'photo']
